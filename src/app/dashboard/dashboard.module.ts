e84513766af8c8f515328babe95408d94e965b38
Planetoide <adcircus@gmail.com>
2021-02-28 20:59:08 -0600

    se agrego navegacion en dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,7 +3,9 @@ import { UsersComponent } from './users/users.component';
 import { SalesComponent } from './sales/sales.component';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 const routes: Routes = [
   {
     path: '',
@@ -23,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [DashboardComponent, UsersComponent, SalesComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), MatToolbarModule, MatIconModule, MatButtonModule],
   exports: [RouterModule]
 })
 export class DashboardModule { }
